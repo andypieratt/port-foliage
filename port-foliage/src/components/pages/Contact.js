89c10548edfc3a5827c43154ee1cdbdfc9b64414
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-08 14:48:03 -0600

    added the the contact function to the contact page that will return the contact page when clicked through the nav link

---
+++ port-foliage/src/components/pages/Contact.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Please contact or connect with me through the links below:</p>
+      <ul>
+        <li>
+          GitHub:
+          <a
+            href="https://github.com/andypieratt"
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            @andypieratt
+          </a>
+        </li>
+        <li>
+          LinkedIn:
+          <a
+            href="https://www.linkedin.com/in/andrew-pieratt/"
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
+            Andrew Pieratt LinkedIn
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- port-foliage/src/components/pages/Home.js
+++ port-foliage/src/components/pages/Home.js
@@ -8,8 +8,21 @@ export default function Home() {
       </header>
 
       <h2>About Me:</h2>
-      <img src=""></img>
-      <p></p>
+      <img></img>
+      <p>
+        Hi, my name is Andrew (Andy) Pieratt. In May of 2021 graduated from the
+        University of Colorado - Denver with a Bachelor's Degree in Music
+        Business. In 2022, I decided to pursue a certification from The
+        University of Denver for Coding and Full-Stack Web Development to add to
+        my repetoir of skills. Please navigate to the "Projects" section to view
+        some of my favorite works!
+      </p>
+      <p>
+        I am a very creative person and I try to add that creativity to all of
+        the works I create. I am a Rock/Heavy Metal drummer and I love to spend
+        most of my free-time practicing and creating music. Whatever time I have
+        left, I spend collecting and sourcing vintage clothing and furniture.
+      </p>
     </div>
   );
 }
