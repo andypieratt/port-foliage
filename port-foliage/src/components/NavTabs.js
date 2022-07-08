92cf4b2c83a39e94f3e9b01d904643da13ac75a3
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-07-08 14:19:58 -0600

    removed intial fies and created react app for this portfolio and added intial files and layout for the single-page app

---
+++ port-foliage/src/components/NavTabs.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul>
+      <li>
+        <a
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li>
+        <a
+          href="#about"
+          onClick={() => handlePageChange("About")}
+          className={currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </a>
+      </li>
+      <li>
+        <a
+          href="#projects"
+          onClick={() => handlePageChange("Projects")}
+          className={
+            currentPage === "Projects" ? "nav-link active" : "nav-link"
+          }
+        >
+          Projects
+        </a>
+      </li>
+      <li>
+        <a
+          href="#contact"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
